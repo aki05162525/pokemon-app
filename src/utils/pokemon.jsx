0aadf2694f3a182c928e5747399fe95d67095709
aki05162525 <akihiro05162525@gmail.com>
2025-02-08 22:27:23 +0900

    ポケモンデータを取得

--- src/utils/pokemon.jsx
+++ src/utils/pokemon.jsx
@@ -1,7 +1,7 @@
 export const getAllPokemon = (url) => {
   return new Promise((resolve, reject) => {
     fetch(url)
-      .then((res) => res.json())
-      .then((data) => resolve(data));
+      .then((res) => res.json()) // fetch でデータを取得し、JSON形式に変換
+      .then((data) => resolve(data)); // JSONデータを resolve で返す
   });
 };
